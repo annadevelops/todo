8f6c6f29e79cd7fc5deab25884ea06896c0ecde8
Anna N <csofenn@gmail.com>
2022-01-18 15:15:27 +0000

    add Heroku port to app.js

--- todo.js
+++ todo.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { toUnicode } = require('punycode');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const _ = require('lodash');
 const mongoose = require('mongoose');
